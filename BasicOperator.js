190fde035a1a00fd1dae97806a377aff54256c75
Gusti Arsyad <73971893+GustiArsyad123@users.noreply.github.com>
2021-08-21 13:47:47 -0700

    Update BasicOperator.js

--- BasicOperator.js
+++ BasicOperator.js
@@ -6,3 +6,12 @@ console.log(20-8);
 
 // Output the string "4 + 5"
 console.log("4 + 5");
+
+// Output the result of 8 times 4
+console.log(8*4);
+
+// Output the result of 24 divided by 4
+console.log(24/4);
+
+// Output the remainder after dividing 7 by 2
+console.log(7%2);
