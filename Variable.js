ca2b9ecc6968fe8e9858a3f84eb0981f92dab642
Gusti Arsyad <73971893+GustiArsyad123@users.noreply.github.com>
2021-08-21 13:53:42 -0700

    Update Variable.js

--- Variable.js
+++ Variable.js
@@ -3,3 +3,14 @@ let name = "Gusti Arsyad";
 
 // Output the value of the name variable
 console.log(name);
+
+//-------//
+// Declare the length variable
+let length = 5;
+
+// Output the value of the length variable
+console.log(length);
+
+// Use the length variable to output the circle's area
+console.log(length * length * 3);
+
